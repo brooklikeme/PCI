5581a3bcc362bdd8489e4c6a9c2fc0743606abc9
qiuxi zhang <zhangqx2008@gmail.com>
2020-04-28 20:21:22 +0800

    updaate

---
+++ SpotMicro/SpotMicro.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(115200);
+}
+
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  Serial.print("Analog: ");
+  Serial.print(val);
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  Serial.print(", Angle: ");
+  Serial.println(val);
+  delay(1000);                           // waits for the servo to get there
+}
